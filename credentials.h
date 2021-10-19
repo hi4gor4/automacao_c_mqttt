29dcc0a769f3e1e24c055b2eb033dafe164778a4
Hiago Almeida <hi4gor4>
2021-10-19 10:05:56 -0300

    alocando topicos

--- credentials.h
+++ credentials.h
@@ -11,13 +11,6 @@
  * For example mine is : cadominna@gmail.com/topic1
  * To add topics, see https://www.maqiatto.com/configure
  */
-#define TOPICLAMPADA1 "hiago23rangel@gmail.com/luz1"
-#define TOPICLAMPADA2 "hiago23rangel@gmail.com/luz2"
-#define TOPICMAX "hiago23rangel@gmail.com/max"
-#define TOPICMIN "hiago23rangel@gmail.com/min"
-#define TOPICTEMP "hiago23rangel@gmail.com/temp"
-#define TOPICACTIVATE "hiago23rangel@gmail.com/alarm1"
-#define TOPICALARM "hiago23rangel@gmail.com/alarm2"
 
 
 #endif /* LINUX_CREDENTIALS */
