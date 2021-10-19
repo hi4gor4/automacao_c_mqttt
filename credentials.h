77235243e10a5e7fd53d4ab2e4ba9cedb6570d79
Hiago Almeida <hi4gor4>
2021-10-19 13:55:28 -0300

    alocando topicos

--- credentials.h
+++ credentials.h
@@ -3,7 +3,7 @@
 
 /* Provide MQTT broker credentials as denoted in maqiatto.com. */
 #define CLIENTID        "hiago"
-#define BROKER_ADDR     "cf46042a14894cb6b01e37c389b92fa2.s1.eu.hivemq.cloud"
+#define BROKER_ADDR     "cf46042a14894cb6b01e37c389b92fa2.s1.eu.hivemq.cloud:8883"
 #define USERNAME        "hiago23rangel@gmail.com"
 #define PASSWORD        "Gh23140304"
 
